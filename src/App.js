3222ee5077d418ad9b9527e73904443f670490fc
ryeongeunkim <ryeongeun.gim@gmail.com>
2023-02-21 22:40:18 +0900

    장바구니 컨텍스트 추가

--- src/App.js
+++ src/App.js
@@ -1,16 +1,17 @@
 import Header from "./components/Layout/Header";
 import Meals from "./components/Meals/Meals";
 import Cart from "./components/Cart/Cart";
+import CartProvider from "./store/CartProvider";
 
 function App() {
   return (
-    <>
+    <CartProvider>
       <Cart />
       <Header />
       <main>
         <Meals />
       </main>
-    </>
+    </CartProvider>
   );
 }
 
