72587e119c177b464209d49486033e7e4331c402
ryeongeunkim <ryeongeun.gim@gmail.com>
2023-05-10 22:13:13 +0900

    주문서 제출 후 장바구니 비우기

--- src/components/Cart/Cart.js
+++ src/components/Cart/Cart.js
@@ -7,9 +7,10 @@ import CartContext from "../../store/cart-context";
 import Checkout from "./Checkout";
 
 const Cart = (props) => {
-  const cartCtx = useContext(CartContext);
-
   const [isCheckout, setIsCheckout] = useState(false);
+  const [isSubmitting, setIsSubmitting] = useState(false);
+  const [didSubmit, setDidSubmit] = useState(false);
+  const cartCtx = useContext(CartContext);
 
   const totalAmount = `$${cartCtx.totalAmount.toFixed(2)}`;
   const hasItems = cartCtx.items.length > 0;
@@ -24,6 +25,23 @@ const Cart = (props) => {
 
   const orderHandler = () => {
     setIsCheckout(true);
+  };
+
+  const submitOrderHandler = async (userData) => {
+    setIsSubmitting(true);
+    await fetch(
+      "https://food-order-app-3a471-default-rtdb.firebaseio.com/orders.json",
+      {
+        method: "POST",
+        body: JSON.stringify({
+          user: userData,
+          orderedItems: cartCtx.items,
+        }),
+      }
+    );
+    setIsSubmitting(false);
+    setDidSubmit(true);
+    cartCtx.clearCart();
   };
 
   const cartItems = (
@@ -41,19 +59,6 @@ const Cart = (props) => {
     </ul>
   );
 
-  const submitOrderHandler = (userData) => {
-    fetch(
-      "https://food-order-app-3a471-default-rtdb.firebaseio.com/orders.json",
-      {
-        method: "POST",
-        body: JSON.stringify({
-          user: userData,
-          orderedItems: cartCtx.items,
-        }),
-      }
-    );
-  };
-
   const modalActions = (
     <div className={classes.actions}>
       <button className={classes["button--alt"]} onClick={props.onClose}>
@@ -67,33 +72,8 @@ const Cart = (props) => {
     </div>
   );
 
-  const addMealHandler = () => {
-    (async () => {
-      for (const meal of cartCtx.items) {
-        const response = await fetch(
-          "https://food-order-app-3a471-default-rtdb.firebaseio.com/orderMeals.json",
-          {
-            method: "POST",
-            body: JSON.stringify(meal),
-            headers: {
-              "Content-Type": "application/json",
-            },
-          }
-        );
-        const data = await response.json();
-        if (data && data.name) {
-          console.log("data = ", data);
-          cartCtx.removeItem(meal.id);
-        }
-        if (!cartCtx.items.length) {
-          props.onClose();
-        }
-      }
-    })();
-  };
-
-  return (
-    <Modal onClose={props.onClose}>
+  const cartModalContent = (
+    <>
       {cartItems}
       <div className={classes.total}>
         <span>Total Amount</span>
@@ -104,6 +84,29 @@ const Cart = (props) => {
       ) : (
         modalActions
       )}
+    </>
+  );
+
+  const isSubmittingModalContent = <p>Sending order data...</p>;
+
+  const didSubmitModalContent = (
+    <>
+      <p>Successfully sent the order!</p>
+      <div className={classes.actions}>
+        <button className={classes.button} onClick={props.onClose}>
+          Close
+        </button>
+      </div>
+    </>
+  );
+
+  return (
+    <Modal onClose={props.onClose}>
+      {!isSubmitting
+        ? !didSubmit
+          ? cartModalContent
+          : didSubmitModalContent
+        : isSubmittingModalContent}
     </Modal>
   );
 };
